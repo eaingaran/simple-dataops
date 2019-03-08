cf95c5f1c9ad127feae7a5f318215fce2e1c9dfd
Aingaran <eaingaran@gmail.com>
2019-03-08 13:48:56 +0530

    Create rollback_sql.py

---
+++ tools/rollback_sql.py
@@ -0,0 +1,42 @@
+import mysql.connector
+import sys
+
+isSuccess = True
+
+try:
+  arguements = sys.argv
+
+  mydb = mysql.connector.connect(
+    host=arguements[1],
+    user=arguements[2],
+    passwd=arguements[3]
+  )
+
+  cursor = mydb.cursor()
+
+  statement = ""
+
+  for line in open(sql_file, 'r'):
+      if re.match(r'--', line):  # ignore sql comment lines
+          continue
+      if not re.search(r'[^-;]+;', line):  # keep appending lines that don't end in ';'
+          statement = statement + line
+      else:  # when you get a line ending in ';' then exec statement and reset for next statement
+          statement = statement + line
+          #print "\n\n[DEBUG] Executing SQL statement:\n%s" % (statement)
+          try:
+              cursor.execute(statement)
+          except (OperationalError, ProgrammingError) as e:
+              isSuccess = False
+          statement = ""
+  
+  if isSuccess:
+    mydb.commit()
+    print("Success", end='')
+  else:
+    mydb.rollback()
+    print("Failed", end='')
+    
+except Exception as e:
+  isSuccess = False
+  print("Failed", end='')
