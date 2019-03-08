ff167ba76b1e441824de087a92c3ea4805dd65c2
Aingaran <eaingaran@gmail.com>
2019-03-08 13:26:03 +0530

    Update execute_sql.py

--- tools/execute_sql.py
+++ tools/execute_sql.py
@@ -1,38 +1,43 @@
 import mysql.connector
 import sys
 
-arguements = sys.argv
-
-mydb = mysql.connector.connect(
-  host=arguements[1],
-  user=arguements[2],
-  passwd=arguements[3]
-)
-
-cursor = mydb.cursor()
-
-statement = ""
-
 isSuccess = true
 
-for line in open(sql_file, 'r'):
-    if re.match(r'--', line):  # ignore sql comment lines
-        continue
-    if not re.search(r'[^-;]+;', line):  # keep appending lines that don't end in ';'
-        statement = statement + line
-    else:  # when you get a line ending in ';' then exec statement and reset for next statement
-        statement = statement + line
-        #print "\n\n[DEBUG] Executing SQL statement:\n%s" % (statement)
-        try:
-            cursor.execute(statement)
-        except (OperationalError, ProgrammingError) as e:
-            isSuccess = false
-            break
-        statement = ""
-        
-if isSuccess:
-  mydb.commit()
-  print("Success")
-else:
-  mydb.rollback()
+try:
+  arguements = sys.argv
+
+  mydb = mysql.connector.connect(
+    host=arguements[1],
+    user=arguements[2],
+    passwd=arguements[3]
+  )
+
+  cursor = mydb.cursor()
+
+  statement = ""
+
+  for line in open(sql_file, 'r'):
+      if re.match(r'--', line):  # ignore sql comment lines
+          continue
+      if not re.search(r'[^-;]+;', line):  # keep appending lines that don't end in ';'
+          statement = statement + line
+      else:  # when you get a line ending in ';' then exec statement and reset for next statement
+          statement = statement + line
+          #print "\n\n[DEBUG] Executing SQL statement:\n%s" % (statement)
+          try:
+              cursor.execute(statement)
+          except (OperationalError, ProgrammingError) as e:
+              isSuccess = false
+              break
+          statement = ""
+  
+  if isSuccess:
+    mydb.commit()
+    print("Success")
+  else:
+    mydb.rollback()
+    print("Failed")
+    
+except Exception as e:
+  isSuccess = false
   print("Failed")
